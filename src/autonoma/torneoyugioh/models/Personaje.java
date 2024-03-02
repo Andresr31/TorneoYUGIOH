a9544bf724188f0fd58d3a38e8486cba5c25d2c6
David Alejandro Ospina <davidossa0110@gmail.com>
2024-03-01 21:41:13 -0500

    Creacion de clase personaje y creacion de sus atributos

---
+++ src/autonoma/torneoyugioh/models/Personaje.java
@@ -0,0 +1,25 @@
+package autonoma.torneoyugioh.models;
+
+/**
+ * Modelo que reprtesenta los datos de una persona
+ * @author DOSSA0110
+ * @version 1.0.0
+ * @since 2024/03/01
+ */
+public class Personaje {
+    // Atributos
+     /**
+     *  Nombre del personaje de la carta
+     */
+    private String nombre;
+     /**
+     *  Cantidad de poder del personaje 
+     */
+    private int poderAtaque;
+     /**
+     *  Capacidad de defensa del personaje
+     */
+    private int capacidadDefensa;
+    // Metodo Constructor
+    // Metodos de acceso
+}
