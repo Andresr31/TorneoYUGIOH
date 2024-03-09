7640c291731eedd853465e2b0be5b0c92a5ffb49
Samuel Esteban Herrera Bedoya <samuel.herrera.bedoya@gmail.com>
2024-03-08 19:23:03 -0500

    Creacion de la clase Jugador.

---
+++ src/autonoma/torneoyugioh/models/Jugador.java
@@ -0,0 +1,11 @@
+package autonoma.torneoyugioh.models;
+/**
+ * Participantes del torneo
+ * @author Samuel Esteban Herrera Bedoya
+ * @version 0.2.0
+ * @since 2024-03-8
+ */
+public class Jugador 
+{
+    
+}
