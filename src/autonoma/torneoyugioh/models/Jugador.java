59513b53bf976a09d76182591c0cbff880bb95f2
Samuel Esteban Herrera Bedoya <samuel.herrera.bedoya@gmail.com>
2024-03-08 19:26:44 -0500

    Añadido el constructor.

--- src/autonoma/torneoyugioh/models/Jugador.java
+++ src/autonoma/torneoyugioh/models/Jugador.java
@@ -10,4 +10,11 @@ public class Jugador
     private String nombre;
     private int numeroVictorias;
     private int puntosExtra;
+
+    public Jugador(String nombre, int numeroVictorias, int puntosExtra) 
+    {
+        this.nombre = nombre;
+        this.numeroVictorias = numeroVictorias;
+        this.puntosExtra = puntosExtra;
+    }
 }
