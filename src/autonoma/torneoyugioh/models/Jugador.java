1b5691e4d0adb585061c173df02c0b1068d7eb2c
Samuel Esteban Herrera Bedoya <samuel.herrera.bedoya@gmail.com>
2024-03-08 19:28:28 -0500

    Creados los Getter's & Setter's.

--- src/autonoma/torneoyugioh/models/Jugador.java
+++ src/autonoma/torneoyugioh/models/Jugador.java
@@ -17,4 +17,33 @@ public class Jugador
         this.numeroVictorias = numeroVictorias;
         this.puntosExtra = puntosExtra;
     }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroVictorias() 
+    {
+        return numeroVictorias;
+    }
+
+    public void setNumeroVictorias(int numeroVictorias) 
+    {
+        this.numeroVictorias = numeroVictorias;
+    }
+
+    public int getPuntosExtra() {
+        return puntosExtra;
+    }
+
+    public void setPuntosExtra(int puntosExtra) 
+    {
+        this.puntosExtra = puntosExtra;
+    }   
 }
